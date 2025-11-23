df392549829b1607a4a598de5a56370dfd39d494
RileyWong26 <rileywongwong@gmail.com>
2025-11-22 23:54:34 -0500

    Endpoint for accessing the backend

--- server/app.py
+++ server/app.py
@@ -1,27 +1,41 @@
 from flask import Flask, request, jsonify
-from supabase_client import supabase
+from main import processTranscript
+import logging
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
+app.logger.setLevel(logging.INFO)
 
-@app.route("/")
-def home():
-    return {"message": "Supabase + Flask connected!"}
+# @app.route("/")
+# def home():
+#     return {"message": "Supabase + Flask connected!"}
 
-@app.route("/agents", methods=["GET"])
-def get_agents():
-    response = supabase.table("agent").select("*").execute()
-    return jsonify(response.data)
+# @app.route("/agents", methods=["GET"])
+# def get_agents():
+#     response = supabase.table("agent").select("*").execute()
+#     return jsonify(response.data)
 
-@app.route("/interactions", methods=["GET"])
-def get_interactions():
-    agent_id = request.args.get("agent_id")
-    if agent_id:
-        response = supabase.table("interaction").select("*").eq("agent_id", agent_id).execute()
-    else:
-        response = supabase.table("interaction").select("*").execute()
-    return jsonify(response.data)
+# @app.route("/interactions", methods=["GET"])
+# def get_interactions():
+#     agent_id = request.args.get("agent_id")
+#     if agent_id:
+#         response = supabase.table("interaction").select("*").eq("agent_id", agent_id).execute()
+#     else:
+#         response = supabase.table("interaction").select("*").execute()
+#     return jsonify(response.data)
 
+# Send transcript to ai
+@app.route("/response", methods=["POST"])
+def post_transcript():
+    app.logger.info("Running")
+    transcript = request.get_json()
+    app.logger.info(f"Object received: {transcript}")
+    file = transcript.get("file")
+    app.logger.info(f"File: {file}")
+    score = processTranscript(file)
 
+    return jsonify({"response": "Uploaded!"})
 
 
 
